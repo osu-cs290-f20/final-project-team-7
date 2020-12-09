bfe627d2df197d004ec087925bb4ab12a2bf50cd
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-12-09 11:46:44 -0800

    Initial implementation of POST /play

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const fs = require('fs');
 
+const HERO_CARDS = require('./heroCards.json');
+const VILLAIN_CARDS = require('./villainCards.json');
+
 const SESSION_COOKIE = "session";
 const SESSION_LEN = 32; // The number of bytes in a session token.
 const PLAYERS_DIR = './data/players';
@@ -33,7 +36,12 @@ for (var i = 0; i < files.length; i++) {
 // Initializes a player.
 function newPlayer() {
     return {
-        requests: 0
+      heroes: [
+        {index: 0, level: 0}
+      ],
+      villains: [
+        {index: 0}
+      ]
     };
 }
 
@@ -74,16 +82,90 @@ function getPlayer(req, res, callback, create) {
     });
 }
 
+/// Looks up cards for a given player (converting from the indexes
+///     in the player object to the names and stats).
+function getCardInfo(player) {
+    return {
+            heroes: player.heroes.map((card) => HERO_CARDS[card.index].levels[card.level]),
+            villains: player.villains.map((card) => VILLAIN_CARDS[card.index])
+    };
+}
+
+/// Returns the stat boost the first card gets when fighting the second.
+function checkStats(first, second) {
+    function checkStat(firstStat, secondStat) {
+        if (firstStat === undefined || secondStat === undefined) return 0;
+        else return firstStat;
+    }
+    return checkStat(first.tech, second.power) +
+        checkStat(first.power, second.brawn) +
+        checkStat(first.brawn, second.tech);
+}
+
 app.use(cookieParser());
+app.use(express.json());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     getPlayer(req, res, (err, player) => {
-        player.requests += 1;
-        res.status(200).render('game', {player: player});
+        res.status(200).render('game', getCardInfo(player));
     }, true);
+});
+
+app.post('/play', (req, res) => {
+    getPlayer(req, res, (err, player) => {
+        if (err) {
+            res.redirect(303, '/');
+            return;
+        }
+        if (req.body && req.body.hero && req.body.villain) {
+            // Get the index of the selected cards in the player's hand
+            var heroIndex = player.heroes.findIndex((card) => HERO_CARDS[card.index].levels[card.level].name === req.body.hero);
+            var villainIndex  = player.villains.findIndex((card) => VILLAIN_CARDS[card.index].name === req.body.villain);
+            if (heroIndex == -1 || villainIndex == -1) {
+                res.status(404).send("You don't have that card.");
+                return;
+            }
+
+            var hero = HERO_CARDS[player.heroes[heroIndex].index].levels[player.heroes[heroIndex].level];
+            var villain = VILLAIN_CARDS[player.villains[villainIndex].index];
+
+            while (true) {
+                var heroDice = Math.floor(Math.random() * 6) + 1;
+                var villDice = Math.floor(Math.random() * 6) + 1;
+                var heroBoost = checkStats(hero, villain);
+                var villBoost = checkStats(villain, hero);
+                var heroScore = heroDice + hero.attack + heroBoost;
+                var villScore = villDice + villain.attack + villBoost;
+
+                if (heroScore == villScore) continue; // Tie; roll again.
+
+                var win = (heroScore > villScore);
+                res.status(200).send({
+                    win: win,
+                    hero: {
+                        dice: heroDice,
+                        attack: hero.attack,
+                        boost: heroBoost,
+                        total: heroScore
+                    },
+                    villain: {
+                        dice: villDice,
+                        attack: villain.attack,
+                        boost: villBoost,
+                        total: villScore
+                    }
+                });
+                break;
+            }
+        } else {
+            res.status(400).send('Invalid request');
+        }
+    });
 });
 
 app.listen(port, () => {
