df78fc614ab446103eff514befda177a47ad9872
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-12-08 18:20:28 -0800

    handlebarize HMTL

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const app = express()
 const port = process.env.PORT || 3000
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.status(200).render('game');
 })
 
 app.listen(port, () => {
