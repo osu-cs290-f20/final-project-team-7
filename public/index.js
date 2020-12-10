c9c247abb868d80691c63800b7e0c598212104af
ll220 <lucylin220@gmail.com>
2020-12-09 19:29:00 -0800

    Messing around with handlebars

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
-var heroCard = Handlebars.templates.heroCard;
-var villainCard = Handlebars.templates.villainCard;
+
+var heroCardTemplate = Handlebars.templates.heroCard;
+// var villainCardTemplate = Handlebars.templates.villainCard;
 
 var heroCardHand = document.getElementsByClassName('hero-card');
 var villainCardHand = document.getElementsByClassName('villain-card');
@@ -7,43 +8,42 @@ var selectedVillainSpot = document.getElementById('villain-card-spot');
 var selectedHeroSpot = document.getElementById('hero-card-spot');
 var playCardButton = document.getElementById('start-turn-button');
 
-console.log(villainCardHand);
+
+
 for (var i = 0; i < heroCardHand.length; i++)   {
     heroCardHand[i].addEventListener('click', selectHeroListener);
 }
 
 function selectHeroListener(event)  {
-    // console.log("hero card clicked")
     var selectedHeroCard = event.currentTarget;
 
     if (selectedHeroSpot.childElementCount == 0) {
         selectedHeroSpot.appendChild(selectedHeroCard);
     }
    else {
-    //    console.log(selectedHeroSpot.children[0])
        selectedHeroSpot.children[0] = selectedHeroCard;
    }
 }
+
+
 
 for (var i = 0; i < villainCardHand.length; i++)    {
     villainCardHand[i].addEventListener('click', selectVillainListener);
 }
 
-function selectVillainListener(event)   {
-    // console.log("villain card clicked")
-    
+function selectVillainListener(event)   {    
     var selectedVillainCard = event.currentTarget;
-
     selectedVillainSpot.firstChild = event.currentTarget;
 
     if (selectedVillainSpot.childElementCount == 0) {
         selectedVillainSpot.appendChild(selectedVillainCard);
     }
    else {
-    //    console.log(selectedVillainSpot.children[0])
        selectedVillainSpot.children[0] = selectedVillainCard;
    }
 }
+
+
 
 playCardButton.addEventListener('click', playCardListener);
 
